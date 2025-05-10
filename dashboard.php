a1467f6a68f1a09ad9ec10a3c99cbe3cba0e67db
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2025-05-10 14:40:24 +0200

    style: Create dashboard page

--- dashboard.php
+++ dashboard.php
@@ -9,6 +9,25 @@
   <style>
     main {
       width: 100%;
+      display: flex;
+      flex-direction: column;
+      gap: 16px;
+    }
+
+    .form-control {
+      flex-direction: row;
+    }
+
+    .add-button {
+      line-height: 1rem !important;
+    }
+
+    .todos {
+      flex-grow: 1;
+    }
+
+    .no-todos-label {
+      color: var(--color-text-grey);
     }
   </style>
 </head>
@@ -17,8 +36,25 @@
   <div id="app">
     <?php include "components/nav.php"; ?>
 
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $input = $_POST["input"];
+    }
+    ?>
+
     <main>
-      <div class="wrapper">Welcome, <span class="capitalize"><?= $user->name ?></span>!</div>
+      <div class="wrapper text-xl">Welcome, <span class="capitalize"><?= $user->name ?></span>!</div>
+
+      <form method="post" action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>">
+        <div class="form-control">
+          <input class="input" name="input" type="text" placeholder="What are you up to today?" />
+          <button class="btn btn-primary add-button text-xl">+</button>
+        </div>
+      </form>
+
+      <div class="todos">
+        <p class="no-todos-label">No todos yet.</p>
+      </div>
     </main>
   </div>
 </body>
